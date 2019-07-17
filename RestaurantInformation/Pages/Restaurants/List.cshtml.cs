f0690047b31bfb4b53562d571bc523da2b9e2575
Rodion Gyrbu <fpsoff@outlook.com>
2019-07-17 19:11:35 +0300

    Logging messages

--- RestaurantInformation/Pages/Restaurants/List.cshtml.cs
+++ RestaurantInformation/Pages/Restaurants/List.cshtml.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using RestaurantInformation.Core;
 using RestaurantInformation.Data;
 
@@ -11,20 +12,23 @@ namespace RestaurantInformation.Pages.Restaurants
     {
         private readonly IConfiguration configuration;
         private readonly IRestaurantData restaurantData;
+        private readonly ILogger<ListModel> logger;
+
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
 
         [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
 
-        public ListModel(IConfiguration configuration, IRestaurantData restaurantData)
+        public ListModel(IConfiguration configuration, IRestaurantData restaurantData, ILogger<ListModel> logger)
         {
             this.configuration = configuration;
             this.restaurantData = restaurantData;
+            this.logger = logger;
         }
         public void OnGet()
         {
-
+            logger.LogError("executing listmodel");
             Message = configuration["Message"];
             Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
         }
